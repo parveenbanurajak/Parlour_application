b640d74cfa4460dd8774f6e661d5bf1208863241
Parveen Banu A <parveenbanu.rajak@gmail.com>
2024-10-20 19:41:35 +0530

    again dusted

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <div className="footer-nav">
         <ul className="footer-links">
           <li><a href="#home">Home</a></li>
-          <li><a href="#whyus">Why You Need Us</a></li>
+          <li><a href="#needforus">Why You Need Us</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact Us</a></li>
         </ul>
@@ -20,10 +20,10 @@ const Footer = () => {
 
       <div className="footer-content">
         <p>&copy; 2024 MAYURI. All rights reserved.</p>
-        <div className="social-icons">
+        {/* <div className="social-icons">
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">Facebook</a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">Instagram</a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
